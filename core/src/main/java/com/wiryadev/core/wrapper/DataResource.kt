f4dc326301c32f00110f3f88bf52992104b1b1d1
wiryadev <abrarwiryawan@gmail.com>
2022-08-28 12:49:20 +0700

    reformat resource class

--- core/src/main/java/com/wiryadev/core/wrapper/DataResource.kt
+++ core/src/main/java/com/wiryadev/core/wrapper/DataResource.kt
@@ -5,6 +5,12 @@ sealed class DataResource<T>(
     val message: String? = null,
     val exception: Exception? = null,
 ) {
+
     class Success<T>(data: T) : DataResource<T>(data)
-    class Error<T>(exception: Exception?, data: T? = null) : DataResource<T>(data, exception = exception)
+
+    class Error<T>(
+        exception: Exception?,
+        data: T? = null,
+    ) : DataResource<T>(data, exception = exception)
+
 }
--- core/src/main/java/com/wiryadev/core/wrapper/ViewResource.kt
+++ core/src/main/java/com/wiryadev/core/wrapper/ViewResource.kt
@@ -5,9 +5,19 @@ sealed class ViewResource<T>(
     val message: String? = null,
     val exception: Exception? = null,
 ) {
+
     class Success<T>(data: T) : ViewResource<T>(data)
-    class Error<T>(exception: Exception?, data: T? = null) :
-        ViewResource<T>(data, exception = exception)
-    class Empty<T>(data: T? = null, msg: String? = null) : ViewResource<T>(data)
+
+    class Error<T>(
+        exception: Exception?,
+        data: T? = null,
+    ) : ViewResource<T>(data, exception = exception)
+
+    class Empty<T>(
+        data: T? = null,
+        msg: String? = null,
+    ) : ViewResource<T>(data)
+
     class Loading<T>(data: T? = null) : ViewResource<T>(data)
+
 }
